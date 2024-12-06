f7fe1fcb94d9124ebef6b428a8eef912051c1dc8
Abhinav872006 <abhinavj9852@gmail.com>
2024-12-06 15:58:50 +0530

    Add files via upload

---
+++ day 10/index.js
@@ -0,0 +1,63 @@
+// // incremwnt the number on every click on click button for eg if initial value is 0
+
+// const counter = document.getElementById("counter");
+
+
+// let count = 0;
+// function incrementvalue(){
+
+//     count++;
+//     counter.innerText = count;
+
+// }
+
+
+
+
+
+
+
+// function decvalue(){
+
+//     count--;
+//     counter.innerText = count;
+
+// }
+
+// const a =5;
+// const b = 10;
+// console.log("---value---",a);
+
+
+// try{
+//     a=6;
+//     console.log("---value of a---",a)
+//     }
+//     catch(error)
+//     {
+//         console.log(error);
+//     }
+
+
+//         console.log("---value of b---",b);
+
+
+//         function mypromisfun(res,rej){
+//             let delay = 1000000000;
+//             while(delay--);
+//             res("Hello World");
+
+            
+//         }
+//         const mypromise = new Promise(mypromisfun);
+
+//         console.log(mypromise);
+
+
+
+const mypromise = new Promise(solve);
+function solve(fun1,fun2){
+
+
+
+} 
